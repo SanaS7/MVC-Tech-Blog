1757e419b1e2b2ccd71596dc12a4675975b36a7e
SanaS7 <sana.sadiq77@gmail.com>
2023-07-06 06:41:31 +0500

    make it better

--- utils/auth.js
+++ utils/auth.js
@@ -1,24 +1,9 @@
-const User = require('../models/User'); // Import the User model or the appropriate MongoDB collection model
-
-const withAuth = async (req, res, next) => {
-  try {
-    // Get the user ID from the session or any other relevant information
-    const userId = req.session.userId;
-
-    // Check if the user is logged in by querying the database
-    const user = await User.findById(userId); // Replace with the appropriate query based on your MongoDB setup
-
-    if (!user) {
-      // User not found, redirect to the login route
-      return res.redirect('/login');
-    }
-
-    // User is authenticated, allow the request to proceed
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
     next();
-  } catch (error) {
-    // Handle any errors that occur during the authentication process
-    console.error('Error authenticating user:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
