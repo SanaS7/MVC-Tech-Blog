e9e878a5fc65ccbcbb046c3b75254222aeb67786
SanaS7 <sana.sadiq77@gmail.com>
2023-07-06 06:34:17 +0500

    make it better

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,8 @@
+const moment = require('moment');
+
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
+    // Format date as MM/DD/YYYY using moment.js
+    return moment(date).format('MM/DD/YYYY');
   }
 };
