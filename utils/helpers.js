69448747dd0fb18fb61acaf3f3b0b19c641a3fb4
SanaS7 <sana.sadiq77@gmail.com>
2023-07-06 06:38:37 +0500

    make it better

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,21 @@
 const moment = require('moment');
 
+// module.exports = {
+//   format_date: (date) => {
+//     // Format date as MM/DD/YYYY using moment.js
+//     return moment(date).format('MM/DD/YYYY');
+//   }
+// };
+
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY using moment.js
-    return moment(date).format('MM/DD/YYYY');
+    // Check if date is undefined or null
+    if (!date) {
+      return ''; // Return an empty string or any other default value you prefer
+    }
+
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
   }
 };
+
